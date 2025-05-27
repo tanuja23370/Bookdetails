aff308eb08c2e6381bc312219498f987d0cb405d
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-27 11:57:48 -0300

    Save

---
+++ BookSales.java
@@ -0,0 +1,24 @@
+package BookDetails;
+
+public class BookSales {
+    String salerName;
+    int numberOfSales;
+    int salerId;
+
+    public BookSales(String salerName, int numberOfSales, int salerId) {
+        this.salerName = salerName;
+        this.numberOfSales = numberOfSales;
+        this.salerId = salerId;
+    }
+
+    public void displaySales() {
+        System.out.println("Saler Name: " + salerName);
+        System.out.println("Number of Sales: " + numberOfSales);
+        System.out.println("Saler ID: " + salerId);
+    }
+
+    public static void main(String[] args) {
+        BookSales sales = new BookSales("Rahul", 250, 103);
+        sales.displaySales();
+    }
+}
