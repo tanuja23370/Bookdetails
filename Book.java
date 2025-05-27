3a1a5f5e0b04fdb6cdd98b3492eb2084c4249ed4
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-27 11:56:21 -0300

    Save

---
+++ Book.java
@@ -0,0 +1,30 @@
+package BookDetails;
+
+public class Book {
+    String bookName;
+    String author;
+    String publisher;
+    double price;
+    int yearOfPublished;
+
+    public Book(String bookName, String author, String publisher, double price, int yearOfPublished) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.yearOfPublished = yearOfPublished;
+    }
+
+    public void display() {
+        System.out.println("Book Name: " + bookName);
+        System.out.println("Author: " + author);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Price: $" + price);
+        System.out.println("Year of Published: " + yearOfPublished);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Effective Java", "Joshua Bloch", "Pearson", 45.99, 2018);
+        book.display();
+    }
+}
